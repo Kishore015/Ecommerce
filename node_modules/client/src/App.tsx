ed38a06a8832f984465e2199aa046db18a00b67d
Kishore <mvskishore15@gmail.com>
2026-04-23 21:49:07 +0530

    scaffold backend

---
+++ node_modules/client/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="container">
+          <div className="left">
+            <h1 className="font-bold">Ecommerce Application with <span>AI capabilities</span></h1>
+          </div>
+        </div>
+      </section>
+        
+    </>
+  )
+}
+
+export default App
